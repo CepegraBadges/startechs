a36fbc4e054f764efaa2b23b4045d02e5cc58452
“Sarah” <sarah@cepegra.be>
2017-08-31 16:18:08 +0200

    integration home page fini

--- htdev/scripts/scripts.js
+++ htdev/scripts/scripts.js
@@ -5,10 +5,11 @@ $(function() {
     }
   })
 
-
-  myInput = document.querySelector(".input_login");
-  myInput.addEventListener("keyup", function() {
+  myInput = document.querySelectorAll(".input_login");
+  for (element of myInput) {
+  element.addEventListener("keyup", function() {
     this.size = this.value.length;
     //alert(this.value.length);
   });
+  }
 });
